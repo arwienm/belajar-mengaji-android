b55a4391d2c60971c8e26cd9135c7738dcd20264
arwienm <arwinmotorejo@gmail.com>
2019-11-13 23:15:50 +0700

    commit file java

--- app/src/main/java/com/arwien/belajarmengaji/HijaiyahActivity.java
+++ app/src/main/java/com/arwien/belajarmengaji/HijaiyahActivity.java
@@ -2,13 +2,38 @@ package com.arwien.belajarmengaji;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.ImageWriter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class HijaiyahActivity extends AppCompatActivity {
+    ImageView TampilGambar;
+    ImageButton show,hide,pindah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hijaiyah);
+
+        TampilGambar = (ImageView)findViewById(R.id.tampil_hijaiyah);
+        show = (ImageView) findViewById(R.id.tampil_hijaiyah);
+        show = (ImageButton) findViewById(R.id.alif);
+        show = (ImageButton) findViewById(R.id.baa);
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TampilGambar.setVisibility(View.VISIBLE);
+            }
+        });
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TampilGambar.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
