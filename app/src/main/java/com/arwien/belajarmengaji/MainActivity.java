42e4ea43307e2314aecc8ee38012c9e2841c9145
arwienm <arwinmotorejo@gmail.com>
2019-11-18 23:47:41 +0700

    menambahkan popup confirm  keluar dan modifikasi frontend

--- app/src/main/java/com/arwien/belajarmengaji/MainActivity.java
+++ app/src/main/java/com/arwien/belajarmengaji/MainActivity.java
@@ -1,16 +1,19 @@
 package com.arwien.belajarmengaji;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton btnExit;
     ImageButton pindah;
     MediaPlayer mp;
 
@@ -18,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btnExit = (ImageButton) findViewById(R.id.btnExit);
 
         //untuk menghilangkan menu bar di Layar depan
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -47,5 +52,35 @@ public class MainActivity extends AppCompatActivity {
                 mp.stop();
             }
         });
+
+        //tombol keluar muncul popup konfirmasi
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Keluar");
+                builder.setMessage("Apakah anda ingin keluar ?");
+                builder.setPositiveButton("Ya, Keluar Sekarang!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+
+                builder.setNegativeButton("Tidak sekarang", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                //moveTaskToBack(true);
+            }
+        });
+
     }
 }
